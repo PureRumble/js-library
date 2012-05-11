0330ba0c46b60428e8a18672ed3cfe8658f794d5
Arash "work" Bazrafshan <work-purerumble@PureMainFrame.(none)>
2012-05-11 14:01:30 +0200

    errors.js: Introduced TestRuntimeError to be used in tests when distinction must be made between intentional test Errors and other possible Errors
    index.js: Exposed TestRuntimeError

--- js/nodejs/ourglobe/testing/index.js
+++ js/nodejs/ourglobe/testing/index.js
@@ -1,5 +1,7 @@
+var TestRuntimeError = require("./errors").TestRuntimeError;
 var Testing = require("./testing").Testing;
 var Tests = require("./tests").Tests;
 
+exports.TestRuntimeError = TestRuntimeError;
 exports.Testing = Testing;
 exports.Tests = Tests;
