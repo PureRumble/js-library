9ef9363a47bb75840d105a549eff3da45e013f97
Arash "work" Bazrafshan <work-purerumble@PureMainFrame.(none)>
2012-06-19 20:19:54 +0200

    Reshuffled files for enabling sharing modules between client and server, but the modules dont work correctly right now as they havent been modified for the new layout

---
+++ js/ourglobe/server/utils/index.js
@@ -0,0 +1,7 @@
+var MoreHttp = require("./morehttp").MoreHttp;
+var MoreMath = require("./moremath").MoreMath;
+var MoreObject = require("./moreobject").MoreObject;
+
+exports.MoreHttp = MoreHttp;
+exports.MoreMath = MoreMath;
+exports.MoreObject = MoreObject;
