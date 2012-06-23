1850d104a49d37d1ea57f105001a7eadafe05e5c
Arash "work" Bazrafshan <work-purerumble@PureMainFrame.(none)>
2012-06-23 20:05:55 +0200

    initnodejs is a module for starting up node-js.
    initnodejs.js: Created script for booting node-js with dojo
    toolkit
    
    initnodejs.js: Set important global variables to be used in
    system
    
    initnodejs.js: Created global funcs ourglobe.require,
    ourglobe.define and ourglobe.declare that wrap the corresponding
    funcs of dojo toolkit
    
    node.sh: Created bash script for convenient startup of node with
    dojo

---
+++ js/ourglobe/server/initnodejs/initnode.js
@@ -0,0 +1,162 @@
+var jsFilePath = process.argv[ 2 ];
+
+dojoConfig =
+{
+	baseUrl:
+		"/home/work-purerumble/files/projects/ourglobe/js/ourglobe",
+	packages:
+	[
+		{ name: "dojo", location: "../og/l/c/dtk/dojo" }
+	]
+};
+
+require( "og/l/c/dtk/dojo/dojo" );
+
+ourglobe = {};
+og = ourglobe;
+
+global.require(
+[ "dojo/_base/declare" ],
+function( declare )
+{
+	var oldReq = global.require;
+	var oldDef = global.define;
+	var oldDec = declare;
+	
+	global.require = require;
+	delete global.define;
+	
+	var resolveDep =
+	function( returnCbVar, cb )
+	{
+		var returnVar =
+			function()
+			{
+				for( var i = 0; i < arguments.length; i++ )
+				{
+					if(
+						typeof( arguments[ i ] ) !== "object" &&
+						typeof( arguments[ i ] ) !== "function"
+					)
+					{
+						throw new Error(
+							requirements[ i ] + " isnt a valid module"
+						);
+					}
+				}
+				
+				var returnVar = cb.apply( cb, arguments );
+				
+				if( returnCbVar === true ) { return returnVar; }
+			}
+		;
+		
+		return returnVar;
+	};
+	
+	ourglobe.require =
+	function( requirements, cb )
+	{
+		if( typeof( requirements ) === "function" )
+		{
+			cb = requirements;
+			requirements = undefined;
+		}
+		
+		if( requirements === undefined )
+		{
+			requirements = [];
+		}
+		
+		if( requirements instanceof Array === false )
+		{
+			throw new Error(
+				"Arg requirements must be an arr but is:\n"+
+				requirements
+			);
+		}
+		
+		if( typeof( cb ) !== "function" )
+		{
+			throw new Error(
+				"Arg cb must be a func but is:\n" + cb
+			);
+		}
+		
+		oldReq( requirements, resolveDep( false, cb ) );
+	};
+	
+	ourglobe.define =
+	function( requirements, cb )
+	{
+		if( typeof( requirements ) === "function" )
+		{
+			cb = requirements;
+			requirements = undefined;
+		}
+		
+		if( requirements === undefined )
+		{
+			requirements = [];
+		}
+		
+		if( requirements instanceof Array === false )
+		{
+			throw new Error(
+				"Arg requirements must be an arr but is:\n"+
+				requirements
+			);
+		}
+		
+		if( typeof( cb ) !== "function" )
+		{
+			throw new Error(
+				"Arg cb must be a func but is:\n" + cb
+			);
+		}
+		
+		oldDef( requirements, resolveDep( true, cb ) );
+	};
+	
+	ourglobe.declare =
+	function( superclass, declaration )
+	{
+		if( typeof( superclass ) === "object" )
+		{
+			declaration = superclass;
+		}
+		
+		if( superclass === undefined )
+		{
+			superclass = null;
+		}
+		
+		if( declaration === undefined )
+		{
+			declaration = {};
+		}
+		
+		if(
+			typeof( superclass ) !== "function" &&
+			superclass !== null
+		)
+		{
+			throw new Error(
+				"Arg superclass must be null or a func but is:\n"+
+				superclass
+			);
+		}
+		
+		if( typeof( declaration ) !== "object" )
+		{
+			throw new Error(
+				"Arg declaration must be an obj but is:\n"+
+				declaration
+			);
+		}
+		
+		return oldDec( superclass, declaration );
+	}
+	
+	require( jsFilePath );
+});
